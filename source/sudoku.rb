4303db8cbce6ea98b1491cb1347ede502e373c1c
davidckim <davidckim320@gmail.com>
2015-01-17 17:38:11 -0800

    Added sudoku solver to portfolio

---
+++ source/sudoku.rb
@@ -0,0 +1,16 @@
+class Sudoku
+  def initialize(board_string)
+  end
+
+  def solve
+  end
+
+  def board
+  end
+
+  # Returns a nicely formatted string representing the current state of the board
+  def to_s
+  end
+end
+
+
